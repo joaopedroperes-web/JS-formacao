c6d3d6e85c6f771fac7ef55973ca9bd141793e82
joaopedroperes-web <jpperesgl@gmail.com>
2023-11-30 17:10:02 -0300

    Finalizando o projeto

--- portifolio/assets/js/main.js
+++ portifolio/assets/js/main.js
@@ -29,9 +29,48 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skills => `<li>${skills}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skills => `<li><img src="${skills.logo}" alt="${skills.name}" title="${skills.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => { 
+        return `
+        <li>
+                        <h3 ${project.github ? 'class="title github"' : ''}>${project.name}</h3>
+                        <a href="${project.url}" target="_blank">${project.url}</a>
+                    </li>
+        `
+        }).join('')
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => { 
+        return `
+        <li>
+            <h3 class="title">${experience.name}</h3>
+            <p class="period">${experience.period}</p>
+            <p>${experience.description}</p>
+        </li>
+        `
+        }).join('')
+}
+
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
 }) ()

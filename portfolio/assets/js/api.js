e3c914f07f5a8d785e11deb352fcfc66c40c1039
joaopedroperes-web <jpperesgl@gmail.com>
2023-12-05 22:20:05 -0300

    Correção de rota

--- portfolio/assets/js/api.js
+++ portfolio/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/joaopedroperes-web/JS-formacao/main/portifolio/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/joaopedroperes-web/JS-formacao/main/portfolio/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
